a12a5138d0e83384dcce661360b86d4d5ef5838b
McBand <30402003+McBand@users.noreply.github.com>
2017-07-24 14:36:17 +0300

    asd

--- css/js.js
+++ css/js.js
@@ -1,1 +1,6 @@
 $('#myHeader h1').html('some staff');
+$('ul').append('<li>coffe</li>');
+$('#addOne').click(function() {
+    var value = $('#impName').val();
+    $('ul').append('<li>'+value+'</li>');
+});
