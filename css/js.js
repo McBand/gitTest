72f5e5ebc1c06d836952cd5c36ebc0797c0ece1e
McBand <30402003+McBand@users.noreply.github.com>
2017-07-26 15:47:35 +0300

    you

--- css/js.js
+++ css/js.js
@@ -1,7 +1,33 @@
 $(function(){
 
-    $('#Ass').on('mouseover', function() {
-        $('#PanelHide').slideToggle(300);
-    });
+    let width = 1000;
+    let animationSpeed = 3000;
+    let pause = 500;
+    let currentSlide = 1;
 
+    let $slider = $('#slider');
+    let $slideContainer = $slider.find('.slides');
+    let $slides = $slideContainer.find('.slide');
+
+    let interval;
+    
+    function startSlider(){
+       interval = setInterval(function(){
+            $slideContainer.animate({'margin-left':'-='+width}, animationSpeed, function(){
+                currentSlide++;
+                if(currentSlide === $slides.length) {
+                    currentSlide = 1;
+                    $slideContainer.css('margin-left', 0);
+                }
+            });
+        }, pause);
+   }
+
+function pauseSlider(){
+    clearInterval(interval);
+}
+    $slider.on('mouseenter', pauseSlider).on('mouseleave', startSlider);
+
+    startSlider();
+  
 });
